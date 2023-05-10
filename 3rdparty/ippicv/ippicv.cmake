81f3908f5d1a5367bb2b47c669417973698f8483
dengshaolin <dengshaolin@malong.com>
2023-05-10 01:54:04 +0000

    add ippicv files
    Signed-off-by: dengshaolin <dengshaolin@malong.com>

--- 3rdparty/ippicv/ippicv.cmake
+++ 3rdparty/ippicv/ippicv.cmake
@@ -39,7 +39,8 @@ function(download_ippicv root_var)
                URL
                  "${OPENCV_IPPICV_URL}"
                  "$ENV{OPENCV_IPPICV_URL}"
-                 "https://raw.githubusercontent.com/opencv/opencv_3rdparty/${IPPICV_COMMIT}/ippicv/"
+		 "file://${CMAKE_CURRENT_SOURCE_DIR}"
+		 #"https://raw.githubusercontent.com/opencv/opencv_3rdparty/${IPPICV_COMMIT}/ippicv/"
                DESTINATION_DIR "${THE_ROOT}"
                ID IPPICV
                STATUS res
